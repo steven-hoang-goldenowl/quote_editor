2169890f03b74a8abdba47ebc11fdb20c082a56d
steven <baoht@mac-pro.local>
2023-08-23 22:03:02 +0700

    feat: add rubocop

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,8 +5,7 @@ class QuotesController < ApplicationController
     @quotes = Quote.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @quote = Quote.new
@@ -16,18 +15,17 @@ class QuotesController < ApplicationController
     @quote = Quote.new(quote_params)
 
     if @quote.save
-      redirect_to quotes_path, notice: "Quote was successfully created."
+      redirect_to quotes_path, notice: 'Quote was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: "Quote was successfully updated."
+      redirect_to quotes_path, notice: 'Quote was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -35,7 +33,7 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-    redirect_to quotes_path, notice: "Quote was successfully destroyed."
+    redirect_to quotes_path, notice: 'Quote was successfully destroyed.'
   end
 
   private
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
   # test "the truth" do
